9b4e264b8f8a411dc7289f7f371b29132e08867f
Nouman Baloch <nomibaloch775@hotmail.com>
2022-05-19 22:39:10 +0500

    angular client skeleton completed

--- API/Startup.cs
+++ API/Startup.cs
@@ -67,6 +67,8 @@ namespace API
             });
             });
 
+            services.AddCors();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -82,6 +84,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
  
             app.UseAuthorization(); 
 
